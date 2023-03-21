b6e622fd4deed16f33cf3e24a602be8a75b65983
Dj-milka <128476427+Dj-milka@users.noreply.github.com>
2023-03-21 12:47:34 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,69 @@
+let _1 = 0
+let score = 0
+basic.forever(function () {
+    _1 = randint(0, 2)
+    basic.pause(1000)
+    basic.showNumber(score, 200)
+    _1 = 4
+})
+basic.forever(function () {
+    if (_1 == 0) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . # . # #
+            . # . # .
+            . # . # .
+            `)
+    }
+    if (_1 == 1) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            # # . # .
+            . # . # .
+            . # . # .
+            `)
+    }
+    if (_1 == 2) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . # # # .
+            . # . # .
+            . # . # .
+            `)
+    }
+    if (_1 == 3) {
+        basic.clearScreen()
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        if (_1 == 1) {
+            score += 1
+            basic.clearScreen()
+        } else {
+            score += score - 1
+            basic.clearScreen()
+        }
+    }
+    if (input.buttonIsPressed(Button.AB)) {
+        if (_1 == 2) {
+            score += 1
+            basic.clearScreen()
+        } else {
+            score += score - 1
+            basic.clearScreen()
+        }
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        if (_1 == 0) {
+            score += 1
+            basic.clearScreen()
+        } else {
+            score += score - 1
+            basic.clearScreen()
+        }
+    }
+})
