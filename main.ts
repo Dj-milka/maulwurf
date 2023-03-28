2bbfd0c0b315590f20e81a1d71852d61d933f11b
Dj-milka <128476427+Dj-milka@users.noreply.github.com>
2023-03-28 11:53:05 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,75 +1,63 @@
 let _1 = 0
 let score = 0
 basic.forever(function () {
-    _1 = randint(0, 2)
+    _1 = randint(0, 1)
     basic.pause(1000)
-    basic.showNumber(score, 200)
-    _1 = 4
-})
-basic.forever(function () {
-    if (_1 == 0) {
-        basic.showLeds(`
-            . # . # .
-            . # . # .
-            . # . # #
-            . # . # .
-            . # . # .
-            `)
-    }
-    if (_1 == 1) {
-        basic.showLeds(`
-            . # . # .
-            . # . # .
-            # # . # .
-            . # . # .
-            . # . # .
-            `)
-    }
-    if (_1 == 2) {
-        basic.showLeds(`
-            . # . # .
-            . # . # .
-            . # # # .
-            . # . # .
-            . # . # .
-            `)
-    }
-    if (_1 == 3) {
-        basic.clearScreen()
-    }
-})
-basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        if (_1 == 1) {
-            _1 = 4
-            score += 1
-            basic.clearScreen()
-        } else {
-            _1 = 4
-            score += score - 1
-            basic.clearScreen()
-        }
-    }
     if (input.buttonIsPressed(Button.AB)) {
-        if (_1 == 2) {
-            _1 = 4
-            score += 1
-            basic.clearScreen()
-        } else {
-            _1 = 4
-            score += score - 1
-            basic.clearScreen()
+        basic.showNumber(score)
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.AB)) {
+        basic.pause(150)
+    }
+    if (!(input.buttonIsPressed(Button.AB))) {
+        if (_1 == 0) {
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                . . # . #
+                . . # . .
+                . . # . .
+                `)
+        }
+        if (_1 == 1) {
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                # . # . .
+                . . # . .
+                . . # . .
+                `)
         }
     }
-    if (input.buttonIsPressed(Button.B)) {
-        if (_1 == 0) {
-            _1 = 4
-            score += 1
-            basic.clearScreen()
-        } else {
-            _1 = 4
-            score += score - 1
-            basic.clearScreen()
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.AB)) {
+        basic.pause(150)
+    }
+    if (!(input.buttonIsPressed(Button.AB))) {
+        if (input.buttonIsPressed(Button.A)) {
+            if (_1 == 1) {
+                _1 = 4
+                score += 1
+                basic.clearScreen()
+            } else {
+                _1 = 4
+                basic.clearScreen()
+                score = score - 1
+            }
+        }
+        if (input.buttonIsPressed(Button.B)) {
+            if (_1 == 0) {
+                _1 = 4
+                score += 1
+                basic.clearScreen()
+            } else {
+                _1 = 4
+                basic.clearScreen()
+                score = score - 1
+            }
         }
     }
 })
